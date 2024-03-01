99e58f4797f778b4edfa8090a22e1a414e5c2a47
christianm <christian_morabito@hotmail.com>
2024-03-01 15:26:32 +1100

    added registration feature. now working on login/logout feature

--- yard_duty_service/home/views.py
+++ yard_duty_service/home/views.py
@@ -2,8 +2,12 @@ from django.shortcuts import render
 
 
 def about(request):
-    return render(request, 'home/about.html')
+
+    return render(request, 'home/about.html', {'title': 'About'})
 
 
 def home(request):
-    return render(request, 'home/home.html')
+
+    context = {'title': 'Home'}
+
+    return render(request, 'home/home.html', context)
--- yard_duty_service/yard_duty_service/settings.py
+++ yard_duty_service/yard_duty_service/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'crispy_forms',
+    'crispy_bootstrap4',
+    'users.apps.UsersConfig',
     'home.apps.HomeConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -115,10 +118,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap4'
+
