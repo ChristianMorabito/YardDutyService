2ff753d3e0ef80de1fcbe5dcf201963c0a17227d
christianm <christian_morabito@hotmail.com>
2024-03-04 16:42:22 +1100

    added login & logout functionality

--- yard_duty_service/users/urls.py
+++ yard_duty_service/users/urls.py
@@ -6,6 +6,7 @@ from . import views
 urlpatterns = [
 	path('', views.home, name='users-home'),
 	path('register/', views.register, name='users-register'),
-	path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='users-login'),
+	path('login/', auth_views.LoginView.as_view(template_name='users/login.html',
+	                                            redirect_authenticated_user=True), name='users-login'),
 	path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='users-logout')
 ]
